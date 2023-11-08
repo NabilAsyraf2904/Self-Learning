3a02f438d4a01835a76c9d4cda5f6b44c40e2f64
NabilAsyraf2904 <nabilasyraf215@gmail.com>
2023-11-08 21:03:23 +0800

    Question-4

---
+++ SelfLearning/src/Date081123/IndeedQuestion2.java
@@ -0,0 +1,38 @@
+package Date081123;
+
+import java.util.Scanner;
+
+public class IndeedQuestion2 {
+	
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		
+		Q4();
+
+	}
+	
+	public static void Q4()
+	{
+		//write a Java program to find the duplicate characters in a string.
+		int count = 0;
+		String inputString = sc.nextLine();
+        char[] characters = inputString.toCharArray();
+		
+		for (int i= 0; i< characters.length ; i++) {
+			count = 1;
+			
+			for (int j = 0; j < characters.length; j++) {
+				
+				if (characters[i] == characters[j]) {
+					count++;
+				}
+				
+			}
+			if (count > 1) {
+                System.out.println(characters[i] + " has repeated " + count + " times");
+            }
+		}
+	}
+
+}
